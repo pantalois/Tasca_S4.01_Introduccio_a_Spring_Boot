f61b6fe5d869f7b244aab8037adb5c7851d4b892
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-04 12:52:06 +0100

    feat: add basic health check endpoint

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/UserapiApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t01.userapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+
+    @GetMapping("/health")
+    public String test(){
+        return "OK";
+    }
+
+}
