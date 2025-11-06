082a0c82db66a1adea146904fdc1eb7da2fe3940
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-06 12:04:41 +0100

    feat: add user controller class with static list

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,10 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserController {
+    private static List<User> users = new ArrayList<>();
+}
