7bf601f172a03d34e34fe34e30eb71b0d1ebb0c0
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-06 12:30:34 +0100

    feat: get users list

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,10 +1,19 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class UserController {
     private static List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public List<User> listAllUsers() {
+        return users;
+    }
+
 }
