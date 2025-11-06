924ba3d60fe85a934a3d706554f37254e1dc1521
Pantalois <loisgonzalezalonso@gmail.com>
2025-11-06 16:55:54 +0100

    feat: Create user and add it to user´s list. Added toString method in User

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -2,6 +2,8 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,6 +16,14 @@ public class UserController {
     @GetMapping("/users")
     public List<User> listAllUsers() {
         return users;
+    }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody User user) {
+        users.add(user);
+        return "Created user with" +
+                "user name " + user.getName() + " and email " + user.getEmail()
+                + " UUID = " + user.getId() ;
     }
 
 }
--- src/main/java/cat/itacademy/s04/t01/userapi/model/User.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/model/User.java
@@ -6,4 +6,34 @@ public class User {
     private UUID id;
     private String name;
     private String email;
+
+    public User() {
+
+        this.id = UUID.randomUUID();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+    }
 }
